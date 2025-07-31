64c4ef2eff20d77b923cfd2d77521e770529cb0f
Gocile <1873598498@qq.com>
2025-07-31 09:21:02 +0800

    Initialize Commit

---
+++ src/main/java/com/gocile/circile/CircileApplication.java
@@ -0,0 +1,13 @@
+package com.gocile.circile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gocile/circile/CircileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gocile.circile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
